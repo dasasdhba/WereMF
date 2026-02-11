e6130d995a32ec2d4b9cc993b9ce84176e284e7f
dasasdhba <dasasdhba@outlook.com>
2026-02-11 23:52:23 +0800

    famao kirby

--- WereMF/Skill/FaMao.fs
+++ WereMF/Skill/FaMao.fs
@@ -1,14 +1,82 @@
 module WereMF.Skill.FaMao
 
+open FSharpPlus
+open FSharpPlus.Data
 open WereMF.Common
+open WereMF.Module
+open WereMF.Module.Role
 open WereMF.Module.Skill
 open WereMF.Module.Cli
-open WereMF.State
 open WereMF.Role.FaMao
+open WereMF.State
 
 type FaMaoSkill =
-    | FaMaoSkill
+    {
+        Success : bool
+        Healed : bool
+    }
+    static member New () = { Success = false ; Healed = false }
     interface ISkill
+    interface ISkillExecute with
+        member this.Execute sending = monad {
+            let! context = State.get
+            
+            let target = sending |> getRealTarget
+            if target |> isDoged context.Night then
+                let sender = sending |> getSenderName context.Game
+                let recv = target |> getPlayerName context.Game
+                let night = context.Night.AddMessage $"{sender}想给{recv}丢药水，被 doge 挡了"
+                do! State.put { context with Night = night }
+                this
+            else
+                { this with Success = true }
+        }
+    interface ISkillSummary with
+        member this.Priority = -10
+        member this.GetRealTarget sending =
+            sending |> getRealTarget
+        member this.Summarize sending = monad {
+            if this.Success |> not then None else
+            
+            let! context = State.get
+            
+            let target = sending |> getRealTarget
+            let entity = context.Game.GetEntity target
+            let reversed = entity.State.PotionCount >= 1
+            let entity = { entity with State = entity.State |> EntityState.clearMarks }
+            let context = { context with Game = context.Game.UpdateEntity entity }
+            do! State.put context
+            
+            if this.Healed then None else
+            
+            let recv = target |> getPlayerName context.Game
+            sendMessage { Type = Public ; Content = $"{recv}被丢了药水" }
+            
+            if reversed then
+                if entity.Role |> getCharaType = Leaf then
+                    Some {
+                        Target = entity
+                        Request = DeadRequest.New Sudden
+                    }
+                else
+                    sendMessage { Type = Public ; Content = $"{recv}的阵营反转了！" }
+                    let entity = { entity with State.Reversed = true }
+                    let context = { context with Game = context.Game.UpdateEntity entity }
+                    do! State.put context
+                    None
+            else
+
+            let entity = { entity with State = entity.State |> EntityState.addPotion }
+            let context = { context with Game = context.Game.UpdateEntity entity }
+            do! State.put context
+            None
+        }
+    interface ISkillHealDeadSudden with
+        member this.CanHeal () =
+            this.Healed |> not
+        member this.Heal target =
+            sendMessage { Type = Public ; Content = $"但是{target}被救活了" }
+            { this with Healed = true }
 
 // 获取最大投掷数量（第一晚2瓶，之后1瓶）
 let getFaMaoMaxCount (handler: RoleHandler) (entity: Entity) : int =
@@ -34,8 +102,8 @@ let faMaoSendSkill ps (game: GameContext) =
                 >> filterSelectable game
                 >> filterKidnapped ps
     let filter = giveUpOrFilterWith filter
-    let def () = FaMaoSkill :> ISkill
+    let def () = (FaMaoSkill.New ()) :> ISkill
     
-    let createSkill id = Skill.New ps id FaMaoSkill
+    let createSkill id = Skill.New ps id (FaMaoSkill.New ())
     let parser = parseMultiSkill config filter createSkill
     ps |> sendSkillWith title filter parser def
--- WereMF/Skill/Kirby.fs
+++ WereMF/Skill/Kirby.fs
@@ -1,15 +1,76 @@
 module WereMF.Skill.Kirby
 
 open FSharpPlus
+open FSharpPlus.Data
 open WereMF.Common
+open WereMF.Module.Entity
 open WereMF.Module.Role
 open WereMF.Module.Skill
 open WereMF.Module.Cli
-open WereMF.State
+open WereMF.Role.Bind
+open WereMF.Role.Kirby
 
 type KirbySkill =
     | KirbySkill
     interface ISkill
+    interface ISkillExecute with
+        member this.Execute sending = monad {
+            let! context = State.get
+            let source = sending |> getSource
+            let entity = source |> context.Game.GetEntity
+            if sending.Spring.IsSome then
+                sendMessage { Type = ToPlayer entity.Player ; Content = "失败" }
+                this
+            else
+            
+            let target = sending |> getRealTarget
+            let sender = sending |> getSenderName context.Game
+            let recv = target |> getPlayerName context.Game
+            if target |> isDoged context.Night then
+                sendMessage { Type = ToPlayer entity.Player ; Content = "失败" }
+                let night = context.Night.AddMessage $"{sender}想吸入{recv}，被 doge 挡了"
+                do! State.put { context with Night = night }
+                this
+            else
+                let handler = sending |> getHandler
+                let state = context.Night.GetPlayerState target
+                let state = { state with Kirby = Some (source, handler) }
+                let night = context.Night.SetPlayerState state
+                do! State.put { context with Night = night }
+                this
+        }
+    interface ISkillSummary with
+        member this.Priority = 10
+        member this.GetRealTarget sending =
+            sending |> getSource
+        member this.Summarize sending = monad {
+            if sending.Spring.IsSome then None else
+            
+            let! context = State.get
+            let source = sending |> getSource
+            let entity = source |> context.Game.GetEntity
+            let target = sending |> getRealTarget
+            let tEntity = target |> context.Game.GetEntity
+            let state = context.Night.GetPlayerState target
+            let kirby = state.Kirby
+            match kirby with
+            | Some (s, _) when s = source ->
+                let th = tEntity.Role |> getValidHandlers |> List.randomChoiceWith context.Main.Rng
+                let chara = tEntity |> getHandlerCharaType th
+                if chara = Kirby || chara = Leaf then
+                    sendMessage { Type = ToPlayer entity.Player ; Content = "失败" }
+                    None
+                else
+                    sendMessage { Type = ToPlayer entity.Player ; Content = chara.ToString () }
+                    let role = chara |> createRole context.Main.Roll
+                    let handler = sending |> getHandler
+                    let entity = entity |> updateRoleWithHandler
+                                     (fun (k: KirbyRole) -> { k with CopiedRole = Some role })
+                                     handler
+                    do! State.put { context with Game = context.Game.UpdateEntity entity }
+                    None
+            | _ -> None
+        }
 
 let kirbySendSkill ps game =
     let title = "输入一名玩家的编号吸入，输入 0 放弃"
