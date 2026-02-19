d5f42f469f988b39c22506c1eb39f0fe390dcebb
dasasdhba <dasasdhba@outlook.com>
2026-02-20 01:07:24 +0800

    shelang: remove double spring

--- WereMF/Role/SheLang.fs
+++ WereMF/Role/SheLang.fs
@@ -6,26 +6,17 @@ open WereMF.Module.Role
 type SheLangRole =
     {
         LastSelected : SelectionState
-        Disabled : bool option
     }
-    static member New () = { LastSelected = SelectionState.New () ; Disabled = None }
-    member this.IsDisabled ()
-        = this.Disabled.IsSome
+    static member New () = { LastSelected = SelectionState.New () }
     interface IRole with
         member this.Base = {
             CharaType = SheLang
             Priority = 4
             SummaryName = SheLang.ToString ()
         }
-    interface IRoleUpdateOnNightStart with
-        member this.Update () =
-            { this with LastSelected = this.LastSelected.UpdateOnDayStart () }
     interface IRoleUpdateOnDayStart with
         member this.Update () =
-            let disabled = match this.Disabled with
-                            | Some true -> Some false
-                            | _ -> None
-            { this with Disabled = disabled }
+            { this with LastSelected = this.LastSelected.UpdateOnDayStart () }
     interface IRoleUpdateOnDead with
         member this.Update _ =
             { this with LastSelected = SelectionState.New () }
--- WereMF/Skill/SheLang.fs
+++ WereMF/Skill/SheLang.fs
@@ -11,10 +11,9 @@ open WereMF.Role.SheLang
 
 type SheLangSkill =
     {
-        Disabled : bool
         Success : PlayerNightState option
     }
-    static member New() = { Disabled = false ; Success = None }
+    static member New() = { Success = None }
     interface ISkill
     interface ISkillCost with
         member this.Cost sending = monad {
@@ -25,8 +24,7 @@ type SheLangSkill =
             let target = sending |> getRealTarget
             let entity = entity |> updateRoleWithHandler
                              (fun (s: SheLangRole) -> { s with
-                                                         LastSelected = s.LastSelected.Add target
-                                                         Disabled = if this.Disabled then Some true else s.Disabled })
+                                                         LastSelected = s.LastSelected.Add target })
                              handler
             let game = entity |> game.UpdateEntity
             do! State.put ((main, game), night)
@@ -58,60 +56,22 @@ type SheLangSkill =
             this
         }
 
-// 解析铯郎的输入，格式: "玩家ID" 或 "玩家ID1 玩家ID2"
-let parseSheLangInput (input: string) : Result<PlayerId * PlayerId option, string> = monad {
-    let config = {
-        MaxCount = 2
-        MaxCountError = Some "最多选择 2 个玩家"
-        DuplicateError = Some "不能重复选择同一个玩家"
-    }
-    let! results = parseMultiPlayerId config input
-    match results with
-    | GiveUp -> PlayerId 0, None
-    | Targets ids ->
-        match ids.Length with
-        | 1 -> ids[0], None
-        | v when v >= 2 -> ids[0], Some ids[1]
-        | _ -> return! Error "未知格式"
-}
-
 let sheLangSendSkill ps (game: GameContext) =
     let entity = game.GetEntity ps.Source
-    let lastSelected, isDisabled = 
+    let last =
         match ps.Handler.GetFromEntity entity with
-        | :? SheLangRole as sl -> sl.LastSelected.Selected, sl.IsDisabled()
-        | _ -> [], false
-    
+        | :? SheLangRole as se -> se.LastSelected.Selected
+        | _ -> []
+        
     let title = "输入要扔弹簧的玩家编号（最多 2 个），输入 0 放弃"
-    
     let filter = filterNonExists game
-                    >> filterDead game
-                    >> filterExceptIndexList lastSelected "不能连续对同一个玩家使用弹簧"
-                    >> filterSelectable game
-                    >> filterKidnapped ps
-                    >> (if isDisabled then filterDisabled "你上晚发了两个弹簧，今晚无法行动" else id)
+                >> filterDead game
+                >> filterSelectable game
+                >> filterKidnapped ps
+                >> filterExceptIndexList last "不能连续对同一个玩家使用弹簧"
     let filter = giveUpOrFilterWith filter
-    let def () = SheLangSkill.New () :> ISkill
-    
-    let parser (input: string) : Result<Skill option list, string> = monad {
-        let! results = parseSheLangInput input
-        match results with
-        | PlayerId v, _ when v <= 0 ->
-            [ None ]
-        | target1, target2 ->
-            let! target1 = Ok target1 |> filter
-            let target2 = target2 |> Option.map (fun t -> Ok t |> filter)
-            match target2 with
-            | None ->
-                let skill = Skill.New ps target1 (SheLangSkill.New())
-                [ skill |> Some ]
-            | Some r ->
-                let! t2 = r
-                let s = [ Skill.New ps target1 (SheLangSkill.New()) |> Some ]
-                let s = 
-                    if t2 <= PlayerId 0 then s
-                    else s @ [ Skill.New ps t2 { Disabled = true ; Success = None } |> Some ]
-                s
-    }
-    
+    let def () = (SheLangSkill.New ()) :> ISkill
+    let parser = parsePlayerId >> filter >> Result.map (
+        fun r -> if r <= PlayerId 0 then [ None ]
+                 else [ Skill.New ps r (SheLangSkill.New ()) |> Some ])
     ps |> sendSkillWith title filter parser def
