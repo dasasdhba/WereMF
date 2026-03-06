280605cd7d0e823018e8bf49b12f2c531d45ec4c
dasasdhba <dasasdhba@outlook.com>
2026-03-06 13:08:15 +0800

    doctor: recover capsule

--- WereMF/Role/Doctor.fs
+++ WereMF/Role/Doctor.fs
@@ -1,15 +1,24 @@
 module WereMF.Role.Doctor
 
 open WereMF.Common
+open WereMF.Module.Role
 
 type DoctorRole =
     {
         Capsule : int
+        Round: int
     }
-    static member New () = { Capsule = 4 }
+    static member New () = { Capsule = 3; Round = 0 }
     interface IRole with
         member this.Base = {
             CharaType = Doctor
             Priority = 0
             SummaryName = Doctor.ToString ()
         }
+    interface IRoleUpdateOnNightStart with
+        member this.Update () =
+            match this.Round with
+            | v when v > 1 ->
+                { this with Round = 1 ; Capsule = 3 }
+            | v ->
+                { this with Round = v + 1 }
